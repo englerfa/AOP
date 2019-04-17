c07bfd069367959f0f27eedf4308d594d72da6f7
englerfa <32197352+englerfa@users.noreply.github.com>
2019-04-17 20:54:18 +0200

    Update Argument.java
    added toString() method

--- Argument.java
+++ Argument.java
@@ -1,11 +1,17 @@
 package aop;
 
 public class Argument {
+	String name;
 	String value;
 	String type;
 	
-	public Argument(String value, String type){
+	public Argument(String name, String value, String type){
+		this.name = name;
 		this.value = value;
 		this.type = type;
+	}
+	
+	public String toString() {
+		return "name=" + name + ", value=" + value + ", type=" + type;
 	}
 }
